088857170582944d7b23a0b0890eec088350c664
Hazen Johnson <leyarotheconquerer@yahoo.com>
2016-04-16 06:22:43 -0500

    Add initial blank Unreal project.

---
+++ Source/LD35/LD35GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LD35GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD35_API ALD35GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
